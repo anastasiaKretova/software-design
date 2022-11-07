3bc88b58adf82a786a91b0d52d488444865d5ab0
sia-kretova <sia-kretova@yandex-team.ru>
2022-11-07 09:22:37 +0300

    clean up

--- java/aop/src/main/java/ru/akirakozov/sd/aop/domain/CustomerManager.java
+++
@@ -1,9 +0,0 @@
-package ru.akirakozov.sd.aop.domain;
-
-/**
- * @author akirakozov
- */
-public interface CustomerManager {
-    int addCustomer(Customer customer);
-    Customer findCustomer(int id);
-}
--- java/test-containers-example/readme.md
+++
@@ -1,7 +0,0 @@
-Build app and add docker image with app to local docker registry:
-
-```mvn -am -pl hello-app package```
-
-Run integration test with docker:
-
-```mvn -am -pl test-example test```
